fa6339332f161c951cb5516c1ce38f75074b95fc
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-25 13:30:20 +0200

    add file home blade and add file about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Homepage</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+    </ul>
+</body>
+</html>
